a00bf39352cd48883a650c80a7afaaabe5663200
Seon <eagle0472@gmail.com>
2022-11-23 19:10:51 +0900

    Revise: 디자인 수정 3 파비콘 추가

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BsWind } from "react-icons/bs";
 
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 import "../fonts/font.css";
 
@@ -16,17 +16,6 @@ function HomePage() {
     </HomePageContainer>
   );
 }
-
-const upAndDownAnimation = keyframes`
-  from
-  {
-    transform: rotate(180deg) translateY(0rem);
-  }
-  to
-  {
-    transform: rotate(180deg) translateY(-2rem);
-  } /* 마지막 위치 */
-`;
 
 const HomePageContainer = styled.div`
   display: flex;
@@ -55,7 +44,16 @@ const HomePageTitleDrawing = styled.span`
 const HomePageIcon = styled(BsWind)`
   font-size: 5rem;
   transform: rotate(180deg);
-  animation: ${upAndDownAnimation} 1s linear 0s infinite alternate;
+  animation: upAndDownAnimation 1s linear 0s infinite alternate;
+
+  @keyframes upAndDownAnimation {
+    from {
+      transform: rotate(180deg) translateY(0rem);
+    }
+    to {
+      transform: rotate(180deg) translateY(-2rem);
+    }
+  }
 `;
 
 export default HomePage;
